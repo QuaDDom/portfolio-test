3b40a5a9e8faafdbb2341227b103b062d5d42ca8
Jadom <matquadev@gmail.com>
2022-08-20 21:56:41 -0300

    [add] TextAnimation components

--- src/components/Sections/Intro.tsx
+++ src/components/Sections/Intro.tsx
@@ -1,20 +1,42 @@
 import React from "react";
 import { useTheme, Text } from "@nextui-org/react";
 import "./Intro.scss";
+import StaggerText from "../Text/StaggerText";
 
 export default function Intro() {
   return (
     <div className="intro section">
       <div className="welcome">
-        <Text size={100} className={"text"}>
-          Hi I'm
-        </Text>
-        <Text size={120} className={"text name"} data-cursor-size={90}>
-          Mateo Leal
-        </Text>
-        <Text size={32} className={"text"}>
-          Fullstack Web Developer
-        </Text>
+        <StaggerText
+          text={"Hi I'm"}
+          fontSize={100}
+          fontFamily={"'Averta-Semibold', serif"}
+          height={140}
+          duration={1}
+          stagger={80}
+        />
+        <StaggerText
+          duration={1}
+          stagger={80}
+          text={"Mateo Leal"}
+          fontSize={120}
+          fontFamily={"'Averta-Semibold', serif"}
+          height={140}
+          stroke
+          data-cursor-size={90}
+          data-cursor-exclusion
+          triggerAfter={1}
+        />
+
+        <StaggerText
+          text={"Fullstack Web Developer"}
+          fontSize={32}
+          fontFamily={"'Averta-Semibold', serif"}
+          height={50}
+          duration={1}
+          stagger={80}
+          triggerAfter={3}
+        />
       </div>
     </div>
   );
---
+++ src/components/Text/StaggerText.tsx
@@ -0,0 +1,158 @@
+import React, { useState, useEffect } from "react";
+import "./StaggerText.scss";
+import { useTheme, Text } from "@nextui-org/react";
+
+const StaggerTextLetter = ({
+  letter,
+  index,
+  reverse,
+  animate,
+  unit,
+  direction,
+  fontSize,
+  wordSpacing,
+  stagger,
+  duration,
+  stroke,
+  ...props
+}: any) => (
+  <Text
+    style={{
+      transition: "0.3s",
+      transform:
+        reverse == null || reverse == "" || reverse == false || reverse !== true
+          ? animate
+            ? `translateY(0${unit == null || unit == "" ? "px" : unit})`
+            : `translateY(${
+                direction == null || direction == "" || direction !== "down"
+                  ? fontSize == null || fontSize == 0
+                    ? 48
+                    : fontSize
+                  : fontSize == null || fontSize == 0
+                  ? -48
+                  : -fontSize
+              }${unit == null || unit == "" ? "px" : unit})`
+          : animate
+          ? `translateY(${
+              direction == null || direction == "" || direction !== "down"
+                ? fontSize == null || fontSize == 0
+                  ? -48
+                  : -fontSize
+                : fontSize == null || fontSize == 0
+                ? 48
+                : fontSize
+            }${unit == null || unit == "" ? "px" : unit})`
+          : `translateY(0${unit == null || unit == "" ? "px" : unit})`,
+      transitionDelay: `${((duration == null || duration == 0
+        ? 0.7
+        : duration) /
+        (stagger == null || stagger == 0 ? 60 : stagger)) *
+        index}s`,
+      transitionDuration: `${
+        duration == null || duration == 0 ? 0.7 : duration
+      }s`,
+      transitionTimingFunction: "ease-in-out",
+      transitionProperty: "transform",
+      fontSize: `${fontSize == 0 || fontSize == null ? 48 : fontSize}${
+        unit == null || unit == "" ? "px" : unit
+      }`,
+    }}
+    className={`stagger-animation-character ${stroke ? "text name" : ""}`}
+    {...props}
+  >
+    {letter === " " ? (
+      <Text
+        className={stroke ? "text name" : ""}
+        css={{
+          fontSize: `${
+            wordSpacing == null || wordSpacing == 0
+              ? fontSize == 0 || fontSize == null
+                ? 48
+                : fontSize
+              : wordSpacing
+          }${unit == null || unit == "" ? "px" : unit}`,
+        }}
+      >
+        &nbsp;
+      </Text>
+    ) : (
+      letter
+    )}
+  </Text>
+);
+
+const StaggerTextReveal = ({
+  text = "<text placeholder>",
+  triggerAfter = 0,
+  height,
+  fontSize,
+  unit,
+  width,
+  reverse,
+  direction,
+  duration,
+  stagger,
+  wordSpacing,
+  style,
+  stroke,
+  ...props
+}: any) => {
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimate(true);
+    }, triggerAfter);
+  });
+
+  if (typeof text !== "string" || text.length === 0) {
+    throw new Error("Wrong text property");
+  }
+
+  const lettersComponents = [];
+
+  for (let i = 0; i < text.length; i++) {
+    lettersComponents.push(
+      <StaggerTextLetter
+        key={i}
+        letter={text[i]}
+        index={i}
+        reverse={reverse}
+        animate={animate}
+        unit={unit}
+        direction={direction}
+        fontSize={fontSize}
+        wordSpacing={wordSpacing}
+        stagger={stagger}
+        duration={duration}
+        stroke={stroke}
+        {...props}
+      />
+    );
+  }
+
+  return (
+    <div
+      //className="stagger-animation-text"
+      style={{
+        ...style,
+        overflow: "hidden",
+        height: `${
+          height == 0 || height == null
+            ? fontSize == 0 || fontSize == null
+              ? 48
+              : fontSize
+            : height
+        }${unit == null || unit == "" ? "px" : unit}`,
+        width: `${width == 0 || width == null ? "" : width}${
+          unit == null || unit == "" ? "px" : unit
+        }`,
+        display: "flex",
+      }}
+    >
+      {lettersComponents}
+    </div>
+  );
+};
+
+export default StaggerTextReveal;
