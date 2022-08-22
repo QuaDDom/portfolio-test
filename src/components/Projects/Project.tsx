fa938105610a14c21a8c2ab3fcb59c3b8d777e94
Jadom <matquadev@gmail.com>
2022-08-22 16:18:29 -0300

    [add] Projects & Works components

---
+++ src/components/Projects/Project.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Project.scss";
+
+export default function Project() {
+  return <div>Project</div>;
+}
---
+++ src/components/Projects/ViewProjects.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./ViewProjects.scss";
+
+export default function ViewProjects() {
+  return <div>ViewProjects</div>;
+}
